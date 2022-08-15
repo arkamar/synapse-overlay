arkamar/synapse-overlay

commit 6a3d5a0e406de1e68c82535f65c748c87beb0e30

dev-python/frozendict: live ebuild
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

dev-python/frozendict/frozendict-9999.ebuild
@@ -1,0 +1,24 @@
+# Copyright 2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DISTUTILS_USE_PEP517=setuptools
+PYTHON_COMPAT=( python3_{9..10} )
+
+inherit distutils-r1 git-r3
+
+DESCRIPTION="A simple immutable mapping for python"
+HOMEPAGE="
+	https://github.com/Marco-Sulla/python-frozendict/
+	https://pypi.org/project/frozendict/
+"
+EGIT_REPO_URI="https://github.com/Marco-Sulla/python-frozendict/"
+
+LICENSE="MIT"
+SLOT="0"
+
+python_test() {
+	cd "${T}" || die
+	epytest "${S}/test"
+}

commit 398a30c6e9ff293d00ba6361a5f2c6229e833f97

dev-python/frozendict: fix pytest dependencies
This fix is related to similar fix in ::gentoo ffa1292e771b
("dev-python/frozendict: fix pytest dependencies").

Fixes: 6a3d5a0e406d ("dev-python/frozendict: live ebuild")
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

dev-python/frozendict/frozendict-9999.ebuild
@@ -18,6 +18,8 @@ EGIT_REPO_URI="https://github.com/Marco-Sulla/python-frozendict/"
 LICENSE="MIT"
 SLOT="0"
 
+distutils_enable_tests pytest
+
 python_test() {
 	cd "${T}" || die
 	epytest "${S}/test"